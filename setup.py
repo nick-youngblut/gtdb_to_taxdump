038f0f651d1c4340de25c24e8ba48e26d749198e
maxibor <maxime.borry@gmail.com>
2022-04-14 14:27:23 +0200

    __version__ read for setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,23 @@ from setuptools import setup, find_packages
 import os
 import glob
 import numpy
-from bin import __version__
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
     
 # dependencies
@@ -15,7 +31,7 @@ install_reqs = [
 desc = 'GTDB database utility scripts'
 setup(
     name = 'gtdb_to_taxdump',
-    version = '0.1.7',
+    version = get_version("bin/__init__.py"),
     description = desc,
     long_description = desc + '\n See README for more information.',
     author = 'Nick Youngblut',
